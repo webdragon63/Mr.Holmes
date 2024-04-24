805cb8a55cb218d820c67e8c3d07b39b807d0fb6
webdragon63 <117004849+webdragon63@users.noreply.github.com>
2024-04-24 06:02:01 +0000

    Create index.php

---
+++ GUI/index.php
@@ -0,0 +1,8 @@
+<?php
+/*ORIGINAL CREATOR: Luca Garofalo (Lucksi)
+AUTHOR: Luca Garofalo (Lucksi)
+Copyright (C) 2021-2023 Lucksi <lukege287@gmail.com>
+License: GNU General Public License v3.0*/ 
+
+require("Actions/Login_Controller.php");
+?>
